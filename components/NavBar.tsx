ff66e2de37ad3f7d80399522056a1a10bf2e3d2d
Eugen Mamaj <genimamaj2004@gmail.com>
2024-03-06 22:55:18 +0100

    Styled the links for the navbar to differ from active and non-active.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,20 +7,43 @@ import {
   Link,
 } from "@nextui-org/react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 interface Params {
   authButton: any;
 }
 
 export default function NavBar(params: Params) {
+  const handleActivePage = (page: string) => {
+    const pathname: string = usePathname();
+    if (pathname == page) return true;
+
+    return false;
+  };
 
   return (
-    <Navbar isBordered>
+    <Navbar
+      isBordered
+      classNames={{
+        item: [
+          "flex",
+          "relative",
+          "h-full",
+          "items-center",
+          "data-[active=true]:after:content-['']",
+          "data-[active=true]:after:absolute",
+          "data-[active=true]:after:bottom-0",
+          "data-[active=true]:after:left-0",
+          "data-[active=true]:after:right-0",
+          "data-[active=true]:after:h-[2px]",
+          "data-[active=true]:after:rounded-[2px]",
+          "data-[active=true]:after:bg-orange-500",
+        ],
+      }}
+    >
       <NavbarBrand>
         <Link href="/">
-          <p className="font-bold px-2 text-inherit">
-            StockLib
-          </p>
+          <p className="font-bold px-2 text-inherit">StockLib</p>
           <Image
             className="h-8 w-auto bg-white rounded-md"
             width={150}
@@ -31,28 +54,23 @@ export default function NavBar(params: Params) {
         </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
+        <NavbarItem isActive={handleActivePage("/news")}>
           <Link color="foreground" href="/news">
-            Market News
+            News
           </Link>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem isActive={handleActivePage("/stock-list")}>
           <Link color="foreground" href="/stock-list">
             Stock List
           </Link>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem isActive={handleActivePage("/watchlist")}>
           <Link color="foreground" href="/watchlist">
             Watchlist
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="flex">
-          {params.authButton}
-        </NavbarItem>
-      </NavbarContent>
+      <NavbarContent justify="end">{params.authButton}</NavbarContent>
     </Navbar>
   );
 }
-
