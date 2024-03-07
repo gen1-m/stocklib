a5adfe1429dd56a77403b8679e3601d4fdc21b44
Eugen Mamaj <genimamaj2004@gmail.com>
2024-03-07 23:31:31 +0100

    Installed nodemon.

--- app/websocket/websocket-com.tsx
+++ app/websocket/websocket-com.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import useSocket from "../hooks/useSocket";
 
 export default function WebsocketCom() {
@@ -14,27 +14,33 @@ export default function WebsocketCom() {
     if (!socket) {
       return;
     }
-    // connection is successful => display socket id 
+    // connection is successful => display socket id
     socket.on("connect", () => {
       console.log("client id: ", socket.id);
-    })
+    });
 
     socket.on("responseEvent", (data) => {
       setButtonSMS("Received response from the server:" + data);
-      console.log(data);
-    })
+    });
 
+    // this is for when the client wants to use another page
+    // or when the client just leaves from the site alltogether
+    return () => {
+      socket.disconnect();
+    };
   }, [socket]);
 
-  const sendSocketEvent= () => {
-    socket?.emit('myevent', "Hello server!");
-  }
+  const sendSocketEvent = () => {
+    socket?.emit("myevent", "Hello server!");
+  };
 
   return (
     <>
-      <button className="bg-orange-400" onClick={sendSocketEvent}>
-        {buttonSMS}
+      <button className="m-7" onClick={sendSocketEvent}>
+        <div className="p-2 rounded-xl bg-orange-400 text-black">
+          {buttonSMS}
+        </div>
       </button>
     </>
-  )
+  );
 }
