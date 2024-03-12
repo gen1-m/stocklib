987885722158860a95f6be6009cb9988618c013a
Eugen Mamaj <genimamaj2004@gmail.com>
2024-03-12 10:24:53 +0100

    After setting up the server using GCP, updated the useSocket hook with it's ip address.

--- app/hooks/useSocket.ts
+++ app/hooks/useSocket.ts
@@ -4,7 +4,7 @@ import { Socket, io } from "socket.io-client";
 export default function useSocket() {
   const [socket, setSocket] = useState<Socket | null>(null);
   useEffect(() => {
-    setSocket(io("http://localhost:5000"));
+    setSocket(io("http://35.246.249.132:5000"));
     return () => {
       socket?.disconnect();
     };
