baca4e8dae7ce2086931f66374a82c1fafd330cb
Eugen Mamaj <genimamaj2004@gmail.com>
2024-03-13 13:23:24 +0100

    Installed dotenv module, because apparently I was reading the token from .env wrongly. Now it's fixed the right way.

--- finhub_socket/websocket.ts
+++ finhub_socket/websocket.ts
@@ -1,8 +1,10 @@
+import * as dotenv from "dotenv";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { WebSocket } from 'ws';
 
+dotenv.config({ path: ".env.local" });
 const app = express();
 const httpServer = createServer(app);
 
