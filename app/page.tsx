c83ec5030b14d1d3fa0cda1737c3253a08b92760
Eugen Mamaj <genimamaj2004@gmail.com>
2024-03-15 10:40:22 +0100

    Finished the dummy home page for now, it works, going to style it later. Also implemented tsParticles because why not make it dynamic (allocated ha ha).

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AuthButton from "@/components/AuthButton"
+import MyParticles from "@/components/MyParticles";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
@@ -15,12 +16,13 @@ export default async function Home() {
   }
 
   return (
-    <>
-      <h1 className="text-3xl text-center p-2">
-        Welcome to StockLib
+    <main className="flex flex-col items-center justify-center gap-y-10 my-auto">
+      <MyParticles />
+      <h1 className="text-5xl text-center p-2 max-w-2xl font-semibold">
+        Choose your stocks to invest, then reap the success
       </h1>
-      <br />
       <AuthButton />
-    </>
+    </main>
   )
 }
+
