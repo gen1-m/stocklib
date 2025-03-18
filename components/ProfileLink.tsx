f019ad25a2d7c2ff9a77efa7b1ec13c7a3e427a5
Eugen Mamaj <108270526+gen1-m@users.noreply.github.com>
2025-03-19 00:42:43 +0100

    add: created a link to the profile page

---
+++ components/ProfileLink.tsx
@@ -0,0 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+
+export default async function ProfileLink() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user ? (
+    <Link href="/profile" color="foreground">
+      Profile
+    </Link>
+  ) : (
+    <></>
+  );
+}
